fed077742ad8f134de78254e353b0e98ce831691
lakshya17 <klakshya170@gmail.com>
2020-12-18 15:59:02 +0530

    adding the files to the repo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './navbar.js'
+import './App.css'
+import Page from './page.js'
+const App = () => {
+  return (
+    <div>
+      < Navbar />
+     <div>
+      < Page />
+      
+      </div>
+      </div>
+  );
+}
+
+export default App;
